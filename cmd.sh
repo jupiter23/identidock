82ce9e0634c881729ff48827956a460d8a6cb522
jmlavarenne <jmlavarenne@cdpq.com>
2017-09-08 06:57:24 -0400

    Update message

--- cmd.sh
+++ cmd.sh
@@ -6,7 +6,7 @@ if [ "$ENV" = 'DEV' ]; then
   exec python "identidock.py"
 elif [ "$ENV" = 'UNIT' ]; then
   echo "Running unit tests--"
-  exec python "tests.py"
+  exec python "identidock.py"
 else
   echo "Running production server"
   exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
