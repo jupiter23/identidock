cbc41af615b6b4883e63436678e8708bee8c1145
jmlavarenne <jmlavarenne@cdpq.com>
2017-09-08 06:53:08 -0400

    Update message

--- cmd.sh
+++ cmd.sh
@@ -5,7 +5,7 @@ if [ "$ENV" = 'DEV' ]; then
   echo "Running development server"
   exec python "identidock.py"
 elif [ "$ENV" = 'UNIT' ]; then
-  echo "Running unit tests"
+  echo "Running unit tests--"
   exec python "tests.py"
 else
   echo "Running production server"
